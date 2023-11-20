e8413c0b9f854da6b0681f719dd6d49ec7d11d7f
heltonsantos001 <heltonsanto001@gmail.com>
2023-11-20 15:22:06 -0300

    gg

---
+++ carrinho.js
@@ -0,0 +1,53 @@
+const incrementar = (element, itemId) => {
+    let quantidadeElement = element.parentNode.querySelector('input[type="number"]');
+    quantidadeElement.value++;
+
+    if (quantidadeElement.value >= 10) {
+        quantidadeElement.value = 10;
+    }
+};
+
+const decrementar = (element, itemId) => {
+    let quantidadeElement = element.parentNode.querySelector('input[type="number"]');
+    quantidadeElement.value--;
+
+    if (quantidadeElement.value < 0) {
+        quantidadeElement.value = 0;
+    }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const incrementarButtons = document.querySelectorAll('.incrementar');
+    const decrementarButtons = document.querySelectorAll('.decrementar');
+
+    incrementarButtons.forEach(button => {
+        button.addEventListener('click', () => incrementar(button));
+    });
+
+    decrementarButtons.forEach(button => {
+        button.addEventListener('click', () => decrementar(button));
+    });
+});
+
+// Função para adicionar ao carrinho
+const adicionarAoCarrinho = (itemId) => {
+    // Obtenha informações do item
+    const titulo = document.getElementById(itemId).querySelector('.titulo').innerText;
+    const descricao = document.getElementById(itemId).querySelector('.descricao').innerText;
+    const precoText = document.getElementById(itemId).querySelector('span').innerText;
+    const quantidade = document.getElementById(itemId).querySelector('input[type="number"]').value
+
+    const preco = parseFloat(precoText)
+
+    const newProco = preco * quantidade
+    
+    const produto = {
+        titulo,
+        descricao,
+        preco: newProco,
+        quantidade,
+    }
+    console.log(produto)
+
+
+};
